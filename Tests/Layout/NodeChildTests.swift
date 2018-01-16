a0fce9b76644fa170335b31bfbf6957f44173b0b
Nan Yang <qazyn951230@gmail.com>
2018-01-16 21:01:26 +0800

    Add NodeChildTests

---
+++ Tests/Layout/NodeChildTests.swift
@@ -0,0 +1,52 @@
+// MIT License
+//
+// Copyright (c) 2017 qazyn951230 qazyn951230@gmail.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+@testable import StartPoint
+import XCTest
+
+// Generated from YGNodeChildTest.cpp
+class NodeChildTests: FlexTestCase {
+
+    // Generated from test: reset_layout_when_child_removed
+    func testResetLayoutWhenChildRemoved() {
+        let root = FlexLayout()
+
+        let root_child0 = FlexLayout()
+        root_child0.width(StyleValue.length(100))
+        root_child0.height(StyleValue.length(100))
+        root.append(root_child0)
+
+        root.calculate(direction: Direction.ltr)
+
+        XCTAssertEqual(root_child0.box.left, 0)
+        XCTAssertEqual(root_child0.box.top, 0)
+        XCTAssertEqual(root_child0.box.width, 100)
+        XCTAssertEqual(root_child0.box.height, 100)
+
+        root.remove(root_child0)
+
+        XCTAssertEqual(root_child0.box.left, 0)
+        XCTAssertEqual(root_child0.box.top, 0)
+        XCTAssertTrue(root_child0.box.width.isNaN)
+        XCTAssertTrue(root_child0.box.height.isNaN)
+    }
+}
