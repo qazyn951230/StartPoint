8d6b916f61285efe1b8981dd3ab5b8e9a196675d
Nan Yang <qazyn951230@gmail.com>
2019-02-22 17:54:55 +0800

    UnicodeStream

--- Sources/Stream/UnicodeStream.swift
+++ Sources/Stream/UnicodeStream.swift
@@ -21,13 +21,24 @@
 // SOFTWARE.
 
 public protocol UnicodeProvider {
-    func decode(stream: ByteStream) throws -> UInt32
+    var available: Bool { get }
+    func decode() throws -> UInt32
 }
 
 // http://bjoern.hoehrmann.de/utf-8/decoder/dfa/
 // https://github.com/Tencent/rapidjson/blob/master/include/rapidjson/encodings.h#L204
 public final class UTF8Provider: UnicodeProvider {
-    public func decode(stream: ByteStream) throws -> UInt32 {
+    public let stream: ByteStream
+    
+    public var available: Bool {
+        return stream.available
+    }
+
+    public init(stream: ByteStream) {
+        self.stream = stream
+    }
+
+    public func decode() throws -> UInt32 {
         var byte = stream.peek()
         var value: UInt32 = 0
         var state: UInt32 = 0
@@ -67,44 +78,128 @@ public final class UTF8Provider: UnicodeProvider {
     ]
 }
 
+public final class StringProvider: UnicodeProvider {
+    let scalars: String.UnicodeScalarView
+    var current: String.UnicodeScalarView.Index
+
+    public init(text: String) {
+        scalars = text.unicodeScalars
+        current = scalars.startIndex
+    }
+
+    public var available: Bool {
+        return current < scalars.endIndex
+    }
+
+    public func decode() throws -> UInt32 {
+        guard available else {
+            return 0
+        }
+        defer {
+            current = scalars.index(after: current)
+        }
+        return scalars.element(at: current).value
+    }
+}
+
 public final class UnicodeStream: ReadableStream {
     // Codepoint
     public typealias Value = UInt32
-
-    public private(set) var available: Bool
-    public let stream: ByteStream
+    
     public let provider: UnicodeProvider
-    let data: UnsafeMutablePointer<UInt32>
+    var data: UnsafeMutablePointer<UInt32>
+    let capacity = 2048
     var current: UnsafeMutablePointer<UInt32>
     var index = 0
+    var count = 0
 
-    public init(stream: ByteStream, provider: UnicodeProvider) {
-        self.stream = stream
+    public init(provider: UnicodeProvider) {
         self.provider = provider
-        data = UnsafeMutablePointer<UInt32>.allocate(capacity: 1024)
+        data = UnsafeMutablePointer<UInt32>.allocate(capacity: capacity)
         current = data
-        available = stream.available
+        decode(offset: 0)
     }
 
     deinit {
         data.deallocate()
     }
-    
-    private func decode() {
-        
+
+    public var available: Bool {
+        return index < count
+    }
+
+    // index < count <= capacity
+    // offset > 0: guarantee (index + offset) < count <= capacity
+    private func decode(offset: Int) {
+        precondition(offset > -1 && offset < 1024)
+        guard provider.available else {
+            return
+        }
+        let copy = (offset > 0 && (index + offset) > count) || (count > (capacity / 2))
+        if copy {
+            // uint_32* temp = (uint_8 *)malloc(sizeof(uint_32) * capacity);
+            // memcpy(temp, data, sizeof(uint_32) * (count - index));
+            // free(data);
+            // data = temp;
+            if count > index {
+                let temp = UnsafeMutablePointer<UInt32>.allocate(capacity: capacity)
+                // FIXME: moveAssign == memcpy ?
+                temp.moveAssign(from: data, count: count - index)
+                data.deallocate()
+                data = temp
+                current = data
+                count = count - index
+                index = 0
+            } else {
+                current = data
+                count = 0
+                index = 0
+            }
+        }
+        let size = capacity / 2
+        var pointer = current
+        for _ in count ..< size {
+            if !provider.available {
+                break
+            }
+            // FIXME: try? => try
+            if let codepoint = try? provider.decode() {
+                pointer.initialize(to: codepoint)
+                pointer = pointer.successor()
+                count += 1
+            }
+        }
     }
 
     public func peek() -> UInt32 {
+        decode(offset: 0)
         return available ? current.pointee : 0
     }
 
     public func peek(offset: Int) -> UInt32 {
-        fatalError("peek(offset:) has not been implemented")
+        precondition(offset > -1 && offset < 1024)
+        decode(offset: offset)
+        if index + offset < count {
+            return current.advanced(by: offset).pointee
+        } else {
+            return 0
+        }
     }
 
     public func move() {
+        decode(offset: 0)
+        if available {
+            current = current.successor()
+            index += 1
+        }
     }
 
     public func move(offset: Int) {
+        precondition(offset > -1 && offset < 1024)
+        decode(offset: offset)
+        if index + offset < count {
+            current = current.advanced(by: offset)
+            index += offset
+        }
     }
 }
