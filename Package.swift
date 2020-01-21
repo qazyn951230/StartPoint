936eecf471c416194fb2b436b880aecb5885d863
Nan Yang <qazyn951230@gmail.com>
2020-01-21 16:17:38 +0800

    Package.swift

--- Package.swift
+++ Package.swift
@@ -4,17 +4,29 @@ import PackageDescription
 
 let package = Package(
     name: "StartPoint",
-    dependencies: [],
+    platforms: [.macOS(.v10_14), .iOS(.v10)],
+    products: [
+        .library(name: "StartCore", targets: ["StartCore"]),
+        .library(name: "StartPoint", targets: ["StartPoint"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/qazyn951230/RxSwift.git",
+                 .revision("3f6822956846aeca6a0aae8c933194bf04431f0d"))
+    ],
     targets: [
         .target(
+            name: "Core",
+            dependencies: [],
+            cxxSettings: [.unsafeFlags(["-std=c++17"])]),
+        .target(
             name: "StartCore",
-            dependencies: []),
+            dependencies: ["Core"]),
         .testTarget(
             name: "StartCoreTests",
             dependencies: ["StartCore"]),
         .target(
             name: "StartPoint",
-            dependencies: ["StartCore"]),
+            dependencies: ["StartCore", "RxSwift", "RxCocoa"]),
         .testTarget(
             name: "StartPointTests",
             dependencies: ["StartPoint"]),
